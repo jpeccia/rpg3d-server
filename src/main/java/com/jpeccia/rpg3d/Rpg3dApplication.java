1dfe050271c46859e6fe3abfc5ab20a28a052baf
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-01-17 13:52:39 -0300

    first commit

---
+++ src/main/java/com/jpeccia/rpg3d/Rpg3dApplication.java
@@ -0,0 +1,13 @@
+package com.jpeccia.rpg3d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rpg3dApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rpg3dApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpeccia/rpg3d/Rpg3dApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpeccia.rpg3d;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rpg3dApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
