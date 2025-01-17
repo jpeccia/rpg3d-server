4517a177d5c089c24f8bc8f557d8e7a5d4ee50ba
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-01-17 14:10:20 -0300

    feat: create Scenario Entity

---
+++ src/main/java/com/jpeccia/rpg3d/model/Scenario/Scenario.java
@@ -0,0 +1,40 @@
+package com.jpeccia.rpg3d.model.Scenario;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Scenario {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @ElementCollection
+    private List<String> objects3D;
+
+    @ElementCollection
+    private List<String> lights;
+
+    @Column(nullable = false)
+    private String createdBy;
+
+}
