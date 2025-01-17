34adb61463b2bee0761b128fc8fc3d7b609db455
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-01-17 14:12:47 -0300

    feat: create Map entity

---
+++ src/main/java/com/jpeccia/rpg3d/model/Map/Map.java
@@ -0,0 +1,37 @@
+package com.jpeccia.rpg3d.model.Map;
+
+import com.jpeccia.rpg3d.model.Scenario.Scenario;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Map {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "scenario_id", nullable = false)
+    private Scenario scenario;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Lob
+    private String details;
+
+}
